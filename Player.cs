3737508232c68d572af763259aac298ee22152ab
mekatama <tamatte@hotmail.com>
2019-09-18 22:28:16 +0900

    左右移動

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+	public float speed = 3.0f;	//移動speed
+
+    void Start(){
+    }
+
+    // Update is called once per frame
+    void Update(){
+		//移動
+		if(Input.GetKey("right")){
+			transform.position += transform.right * speed * Time.deltaTime;
+		}
+		if(Input.GetKey("left")){
+			transform.position -= transform.right * speed * Time.deltaTime;
+		}
+    }
+}
