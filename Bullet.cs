004734c67be992b9b86f7463650de2e4567eded6
mekatama <tamatte@hotmail.com>
2019-09-19 19:35:46 +0900

    とりあえず移動

---
+++ Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour{
+	public float speed = 3.0f;	//移動speed
+
+	void Start(){
+	}
+
+	void Update(){
+		//移動
+		transform.position += transform.right * speed * Time.deltaTime;
+	}
+}
