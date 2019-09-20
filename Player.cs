1120d91a66a9c1d171f18ca964bec3550b4866b2
mekatama <tamatte@hotmail.com>
2019-09-20 19:19:05 +0900

    向きflag追加

--- Player.cs
+++ Player.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour{
 	public float speed = 3.0f;	//移動speed
+	public bool isRight;		//向きflag
 
-    void Start(){
-    }
+	void Start(){
+		isRight = true;		//初期化
+	}
 
-    // Update is called once per frame
-    void Update(){
+	void Update(){
 		//移動
 		if(Input.GetKey("right")){
+			isRight = true;
 			transform.position += transform.right * speed * Time.deltaTime;
 		}
 		if(Input.GetKey("left")){
 			transform.position -= transform.right * speed * Time.deltaTime;
+			isRight = false;
 		}
-    }
+	}
 }
