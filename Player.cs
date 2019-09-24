5da92104e8edaa02d343e9bfe773136b3dac8695
mekatama <tamatte@hotmail.com>
2019-09-24 19:28:48 +0900

    左右向くために回転する

--- Player.cs
+++ Player.cs
@@ -13,21 +13,32 @@ public class Player : MonoBehaviour{
 	}
 
 	void Update(){
+		//角度
+		Transform myTransform = this.transform;
+		// ワールド座標を基準に、回転を取得
+		Vector3 worldAngle = myTransform.eulerAngles;
+
 		//右移動
 		if(Input.GetKey("right") && isleft == false){
 			isRight = true;
 			Debug.Log("right");
+			worldAngle.y = 0.0f; // ワールド座標を基準に、y軸を軸にした回転を0度に変更
 			transform.position += transform.right * speed * Time.deltaTime;
 		}else{
 			isRight = false;
 		}
+
 		//左移動
 		if(Input.GetKey("left") && isRight == false){
 			isleft = true;
 			Debug.Log("left");
-			transform.position -= transform.right * speed * Time.deltaTime;
+			worldAngle.y = 180.0f; // ワールド座標を基準に、y軸を軸にした回転を180度に変更
+			transform.position += transform.right * speed * Time.deltaTime;
 		}else{
 			isleft = false;
 		}
+
+		//回転角度確定
+		myTransform.eulerAngles = worldAngle;
 	}
 }
