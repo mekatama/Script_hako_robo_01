f1e218d8a709a5529e6f6989b83fb550ac15e080
mekatama <tamatte@hotmail.com>
2019-09-30 20:19:37 +0900

    方向転換時に一定時間射撃できない処理を入れる

--- Player.cs
+++ Player.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
+	GameObject gameController;			//検索したオブジェクト入れる用
 	public float speed = 3.0f;	//移動speed
 	public bool isRight;		//right flag
 	public bool isLeft;			//left flag
+	private bool isOnceRight;	//一回だけflag
+	private bool isOnceLeft;	//一回だけflag
 
 	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 		isRight = true;		//初期化
 		isLeft = false;		//初期化
+		isOnceRight = false;//初期化
+		isOnceLeft = false;	//初期化
 	}
 
 	void Update(){
@@ -21,9 +27,15 @@ public class Player : MonoBehaviour{
 		//右移動
 		if(Input.GetKey("right") && isLeft == false){
 			isRight = true;
-			Debug.Log("right");
+			isOnceLeft = false;		//リセット
+//			Debug.Log("right");
 			worldAngle.y = 0.0f; // ワールド座標を基準に、y軸を軸にした回転を0度に変更
 			transform.position += transform.right * speed * Time.deltaTime;
+			//shot判定
+			if(isOnceRight == false){
+				noShot();
+				isOnceRight = true;		//一回だけon
+			}
 		}else{
 			isRight = false;
 		}
@@ -31,9 +43,15 @@ public class Player : MonoBehaviour{
 		//左移動
 		if(Input.GetKey("left") && isRight == false){
 			isLeft = true;
-			Debug.Log("left");
+			isOnceRight = false;		//リセット
+//			Debug.Log("left");
 			worldAngle.y = 180.0f; // ワールド座標を基準に、y軸を軸にした回転を180度に変更
 			transform.position += transform.right * speed * Time.deltaTime;
+			//shot判定
+			if(isOnceLeft == false){
+				noShot();
+				isOnceLeft = true;		//一回だけon
+			}
 		}else{
 			isLeft = false;
 		}
@@ -41,4 +59,12 @@ public class Player : MonoBehaviour{
 		//回転角度確定
 		myTransform.eulerAngles = worldAngle;
 	}
+
+	void noShot(){
+		Debug.Log("noshot");
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		gc.isShot = false;		//shot不可にする
+	}
+
 }
--- Player_Shot.cs
+++ Player_Shot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Shot : MonoBehaviour{
+	GameObject gameController;			//検索したオブジェクト入れる用
 	public GameObject bulletObject = null;			//弾プレハブ
 	public Transform bulletStartPosition = null;	//弾の発射位置を取得する
 	private float timeElapsed = 0.0f;				//弾の連射間隔カウント用
@@ -10,10 +11,16 @@ public class Player_Shot : MonoBehaviour{
 //	public AudioClip audioClipShot;					//発射 SE
 
 	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 	}
 
 	void Update(){
-		Shot();
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//発射判定
+		if(gc.isShot){
+			Shot();
+		}
 	}
 
 	void Shot(){
