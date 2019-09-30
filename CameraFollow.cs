c7d8fef9b03a2d4aa1741dce483209bb7025149a
mekatama <tamatte@hotmail.com>
2019-09-30 20:18:18 +0900

    playerにカメラ追従させる

---
+++ CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour{
+	Vector3 diff;
+	public GameObject target;	//追従するオブジェクト
+	public float followSpeed;	//追従スピード
+
+	void Start () {
+		diff = target.transform.position - transform.position;
+	}
+	
+	void Update () {
+		//カメラ追従
+		transform.position = Vector3.Lerp(
+			transform.position,
+			target.transform.position - diff,
+			Time.deltaTime * followSpeed
+		);
+	}
+}
