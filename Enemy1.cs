c85a32b1cecdf2a0cb09326ceb7d3163a560669c
mekatama <tamatte@hotmail.com>
2019-10-02 20:01:43 +0900

    弾に当たったら消す

---
+++ Enemy1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour{
+	void Start(){
+    }
+
+    void Update(){
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter( Collider other) {
+		if(other.tag == "Bullet"){
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+	}
+}
