771c1840b1be7617cb054d73d650980a19fba304
mekatama <tamatte@hotmail.com>
2019-10-02 20:01:09 +0900

    敵との判定追加

--- Bullet.cs
+++ Bullet.cs
@@ -4,19 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour{
 	public float speed = 0.0f;	//移動speed
-	GameObject player;			//検索したオブジェクト入れる用
 
 	void Start(){
-		player = GameObject.FindWithTag ("Player");	//Playerタグのオブジェクトを探す
-		//pって仮の変数にPlayerコンポーネントを入れる
-		Player p = player.GetComponent<Player>();
-		//生成時に移動方向決定
-		if(p.isRight){
-			speed = 1 * speed;
-		}
-		if(p.isLeft){
-			speed = -1 * speed;
-		}
 	}
 
 	void Update(){
@@ -27,6 +16,9 @@ public class Bullet : MonoBehaviour{
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter( Collider other) {
 		if(other.tag == "Wall"){
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+		if(other.tag == "Enemy"){
 			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
 		}
 	}
