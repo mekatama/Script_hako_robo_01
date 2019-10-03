8c67b513090a101e788ab9590e3cbcfe1be83751
mekatama <tamatte@hotmail.com>
2019-10-03 20:17:15 +0900

    enemy HP追加

--- Enemy1.cs
+++ Enemy1.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy1 : MonoBehaviour{
-	void Start(){
-    }
+	public int enemyHp;		//EnemyのHP
+	private bool isDeth;	//死亡flag
 
-    void Update(){
+	void Start(){
+		isDeth = false;		//初期化
 	}
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter( Collider other) {
 		if(other.tag == "Bullet"){
-			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+			//ダメージ処理
+			if(enemyHp > 0){
+				enemyHp = enemyHp - 1;	//攻撃力をHPから引く
+				if(enemyHp <= 0){
+					enemyHp = 0;
+				}
+			}
+			//死亡判定
+			if(enemyHp == 0){
+				if(isDeth == false){
+					Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+					isDeth = true;
+				}
+			}
 		}
 	}
 }
