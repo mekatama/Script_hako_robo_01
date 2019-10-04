4ed9a8f20f8b65891b7b9e43e2cac768044dcdca
mekatama <tamatte@hotmail.com>
2019-10-04 19:30:59 +0900

    enemy移動用のスクリプト作る

---
+++ Enemy1_move.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1_move : MonoBehaviour{
+	public float speed = 0.0f;	//移動speed
+
+	void Update(){
+		//移動
+			transform.position += transform.right * speed * Time.deltaTime;
+	}
+}
