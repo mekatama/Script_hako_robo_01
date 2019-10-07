26b14a5862c2fb315ece59172f40b7092c57b0fc
mekatama <tamatte@hotmail.com>
2019-10-07 19:40:30 +0900

    score変数追加

--- GameController.cs
+++ GameController.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class GameController : MonoBehaviour{
 	private float timeElapsed = 0.0f;	//noShot間隔カウント用
 	public float timeOut;				//noShot間隔の時間
-	public bool isShot;				//shotflag
+	public bool isShot;					//shotflag
+	public int totalScore;				//score
 
 	void Start(){
 		isShot = true;	//初期化
 	}
 
 	void Update(){
+		Debug.Log("score:" + totalScore);
 		//isShotの判定 playが方向転換時に判定して発射を制御
 		if(isShot == false){
 			timeOut = 0.5f;
