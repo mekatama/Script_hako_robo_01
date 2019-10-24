8a8c1114095ff7c8b6268f1cfb5dbfa0f39d5838
mekatama <tamatte@hotmail.com>
2019-10-24 19:36:09 +0900

    title画面制御スクリプト
    start button入れる

---
+++ TitleController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour {
+
+	//Start用の制御関数
+	public void ButtonClicked_Start(){
+		SceneManager.LoadScene("main01");	//シーンのロード
+	}
+}
