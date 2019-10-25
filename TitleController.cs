4ee3aaf67c8c38f3d24a106afe9e865011e24c3e
mekatama <tamatte@hotmail.com>
2019-10-25 20:05:54 +0900

    howtoボタンとreturnボタン追加

--- TitleController.cs
+++ TitleController.cs
@@ -16,6 +16,14 @@ public class TitleController : MonoBehaviour {
 	public void ButtonClicked_Start(){
 		SceneManager.LoadScene("main01");	//シーンのロード
 	}
+	//遊び方画面用の制御関数
+	public void ButtonClicked_HowToPlay(){
+		SceneManager.LoadScene("howtoplay");//シーンのロード
+	}
+	//タイトルに戻る用の制御関数
+	public void ButtonClicked_Return(){
+		SceneManager.LoadScene("title");	//シーンのロード
+	}
 	//アプリ終了
 	public void ButtonClicked_Exit(){
 		Application.Quit();
