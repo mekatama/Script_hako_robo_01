0bcecb8cae57eb3786a3dcb58de5a74b2babac76
mekatama <tamatte@hotmail.com>
2019-10-25 19:50:51 +0900

    exit機能いれる

--- TitleController.cs
+++ TitleController.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class TitleController : MonoBehaviour {
 
+	void Update(){
+		//物理backキー
+		if (Input.GetKeyUp(KeyCode.Escape)){
+			Application.Quit();	//アプリ終了
+		}
+	}
+
 	//Start用の制御関数
 	public void ButtonClicked_Start(){
 		SceneManager.LoadScene("main01");	//シーンのロード
+	}
+	//アプリ終了
+	public void ButtonClicked_Exit(){
+		Application.Quit();
+		Debug.Log("exit");	
 	}
 }
