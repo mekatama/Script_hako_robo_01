b103da0549879a4d4579be21213e76f1f5f86179
mekatama <tamatte@hotmail.com>
2019-11-01 19:13:59 +0900

    子供のwall接触判定を取得

--- Enemy1_move.cs
+++ Enemy1_move.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Enemy1_move : MonoBehaviour{
 	public float speed = 0.0f;	//移動speed
+	private Enemy1 childScript;	//Enemy1オブジェクトを入れる用
+	private bool isOnceRot;		//回転の一回だけ処理
+
+	void Start(){
+		//下の階層のオブジェクト(Enemy1)にアタッチしているスクリプトを参照
+		childScript = GetComponentInChildren<Enemy1>();
+		isOnceRot = false;		//初期化
+	}
 
 	void Update(){
+		//wallで反転
+		if(childScript.isWallHit == true){
+			if(isOnceRot == false){
+				Debug.Log("rot now");
+				isOnceRot =true;
+			}
+		}
 		//移動
 			transform.position += transform.right * speed * Time.deltaTime;
 	}
