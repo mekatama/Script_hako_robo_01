b05236e26839f518c6d4e260e7583a0f0c85d62d
mekatama <tamatte@hotmail.com>
2019-11-08 18:49:39 +0900

    右壁と左壁で回転

--- Enemy1_move.cs
+++ Enemy1_move.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class Enemy1_move : MonoBehaviour{
 	public float speed = 0.0f;	//移動speed
 	private Enemy1 childScript;	//Enemy1オブジェクトを入れる用
-	private bool isOnceRot;		//回転の一回だけ処理
+	private bool isOnceRot_R;	//R回転の一回だけ処理
+	private bool isOnceRot_L;	//L回転の一回だけ処理
 
 	void Start(){
 		//下の階層のオブジェクト(Enemy1)にアタッチしているスクリプトを参照
 		childScript = GetComponentInChildren<Enemy1>();
-		isOnceRot = false;		//初期化
+		isOnceRot_R = false;	//初期化
+		isOnceRot_L = false;	//初期化
 	}
 
 	void Update(){
-		//wallで反転
-		if(childScript.isWallHit == true){
-			if(isOnceRot == false){
-				Debug.Log("rot now");
-				isOnceRot =true;
+		//wall Rで反転
+		if(childScript.isWallHit_R == true){
+			if(isOnceRot_R == false){
+				transform.rotation = Quaternion.Euler(0,180.0f,0);	//回転
+				isOnceRot_R = true;
+				isOnceRot_L = false;
+			}
+		}
+		//wall Lで反転
+		if(childScript.isWallHit_L == true){
+			if(isOnceRot_L == false){
+				transform.rotation = Quaternion.Euler(0,0,0);	//回転
+				isOnceRot_L = true;
+				isOnceRot_R = false;
 			}
 		}
 		//移動
