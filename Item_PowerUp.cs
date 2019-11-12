80544c3483c5001f0eef188fd958fcf98f3394d9
mekatama <tamatte@hotmail.com>
2019-11-12 19:50:37 +0900

    playerと接触判定する

---
+++ Item_PowerUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_PowerUp : MonoBehaviour{
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter( Collider other) {
+		if(other.tag == "Player"){
+			Debug.Log("ItemGet");
+			Destroy(gameObject);	//Item_P削除
+		}
+	}
+}
