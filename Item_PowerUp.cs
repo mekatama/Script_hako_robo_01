44726578aa74b1db084ffe09f9bc451a27317c84
mekatama <tamatte@hotmail.com>
2019-11-13 18:59:03 +0900

    取得時に連射力を加算

--- Item_PowerUp.cs
+++ Item_PowerUp.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item_PowerUp : MonoBehaviour{
+	GameObject gameController;	//検索したオブジェクト入れる用
+	public float rapidNum;		//連射アップ値
+
+	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter( Collider other) {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
 		if(other.tag == "Player"){
 			Debug.Log("ItemGet");
-			Destroy(gameObject);	//Item_P削除
+			gc.rapidFirePower += rapidNum;	//連射アップ
+			Destroy(gameObject);			//Item_P削除
 		}
 	}
 }
