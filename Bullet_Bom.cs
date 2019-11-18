95566126dad79abbf84dd219a1213bad2fb3ee03
mekatama <tamatte@hotmail.com>
2019-11-18 19:33:08 +0900

    bom用の移動スクリプト

---
+++ Bullet_Bom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Bom : MonoBehaviour{
+	public float speed = 0.0f;	//移動speed
+
+	void Start(){
+	}
+
+	void Update(){
+		//移動
+			transform.position += transform.right * speed * Time.deltaTime;
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter( Collider other) {
+		if(other.tag == "Wall_R" || other.tag == "Wall_L"){
+			Destroy(gameObject);	//このGameObjectを［Hierrchy］ビューから削除する
+		}
+	}
+}
