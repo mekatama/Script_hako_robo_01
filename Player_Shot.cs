d3b95e629a7981d8ee3765f18737d803ad3fd7ad
mekatama <tamatte@hotmail.com>
2019-11-20 20:43:39 +0900

    BOMの発射処理を入れる

--- Player_Shot.cs
+++ Player_Shot.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Shot : MonoBehaviour{
-	GameObject gameController;			//検索したオブジェクト入れる用
+	GameObject gameController;						//検索したオブジェクト入れる用
 	public GameObject bulletObject = null;			//弾プレハブ
+	public GameObject bomObject = null;				//ボムプレハブ
 	public Transform bulletStartPosition = null;	//弾の発射位置を取得する
 	private float timeElapsed = 0.0f;				//弾の連射間隔カウント用
 	public float timeOut;							//連射間隔の時間
@@ -17,12 +18,13 @@ public class Player_Shot : MonoBehaviour{
 	void Update(){
 		//gcって仮の変数にGameControllerのコンポーネントを入れる
 		GameController gc = gameController.GetComponent<GameController>();
-		//発射判定
+		//shot発射判定
 		if(gc.isShot){
 			Shot();
 		}
 	}
 
+	//通常弾発射
 	void Shot(){
 		//gcって仮の変数にGameControllerのコンポーネントを入れる
 		GameController gc = gameController.GetComponent<GameController>();
@@ -38,6 +40,22 @@ public class Player_Shot : MonoBehaviour{
 			//弾を生成
 			Instantiate( bulletObject, vecBulletPos, transform.rotation);
 			timeElapsed = 0.0f;
+		}
+	}
+
+	//BOM発射
+	public void Bom(){
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		if(gc.isBom){
+			//SEをその場で鳴らす
+	//		AudioSource.PlayClipAtPoint( audioClipShot, transform.position);	//SE再生(Destroy対策用)
+			//弾を生成する位置を指定
+			Vector3 vecBulletPos = bulletStartPosition.position;
+			//弾を生成
+			Instantiate( bomObject, vecBulletPos, transform.rotation);
+			//残弾減らす
+			gc.bomNum --;
 		}
 	}
 }
