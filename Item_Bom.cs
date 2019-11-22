bc87acfa2a5a2be5e365f8532b97375d6cfe9fd6
mekatama <tamatte@hotmail.com>
2019-11-22 19:35:14 +0900

    item_Bomに残弾+1処理を入れる

---
+++ Item_Bom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Bom : MonoBehaviour{
+	GameObject gameController;	//検索したオブジェクト入れる用
+
+	void Start(){
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter( Collider other) {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		if(other.tag == "Player"){
+			gc.bomNum ++;			//ボム残弾+1
+			Destroy(gameObject);	//Item_Bom削除
+		}
+	}
+}
