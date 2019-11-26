ebcbc3e70c5355a90764425f372b7b7cf217eed1
mekatama <tamatte@hotmail.com>
2019-11-26 20:16:35 +0900

    敵破壊時に出すscore txt制御用

---
+++ DamageTxtDel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTxtDel : MonoBehaviour{
+	void Start () {
+		Destroy(gameObject,1.0f);	//1秒後にオブジェクトの削除
+	}
+}
