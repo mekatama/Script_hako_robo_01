032ab42caf80877d676fc40696047fa5b1f3d905
mekatama <tamatte@hotmail.com>
2019-11-29 21:46:25 +0900

    text反転しないようにする

--- DamageTxtDel.cs
+++ DamageTxtDel.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class DamageTxtDel : MonoBehaviour{
 
 	void Start () {
+		//強制的にy軸０度にして、テキストの反転を防ぐ
+		transform.rotation = Quaternion.AngleAxis(0, new Vector3(0, 1, 0));
 		Destroy(gameObject,1.0f);	//1秒後にオブジェクトの削除
+	}
+
+	void Update(){
 	}
 }
