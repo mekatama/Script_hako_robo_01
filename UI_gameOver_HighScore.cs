6036fdb5cb9a9165445468858bf542be138ca185
mekatama <tamatte@hotmail.com>
2020-01-09 19:48:12 +0900

    未使用highscore表示用

---
+++ UI_gameOver_HighScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_gameOver_HighScore : MonoBehaviour{
+	private int highScore;				//temp
+	public Text highScoreText;			//Textコンポーネント取得用
+
+	void Start(){
+		//HighScoreがなかったら０を入れて初期化
+		highScore = PlayerPrefs.GetInt("HighScore", 0); 
+	}
+
+	void Update () {
+		//highscore表示
+		highScoreText.text = "HighScore : " + highScore.ToString("000000" + "p");
+	}
+}
