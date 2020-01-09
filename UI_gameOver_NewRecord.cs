51e7431abb3bcf9b307f680ef99063aec1643b7e
mekatama <tamatte@hotmail.com>
2020-01-09 19:48:40 +0900

    newrecord字にテキスト表示

---
+++ UI_gameOver_NewRecord.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_gameOver_NewRecord : MonoBehaviour{
+	public Text newRecordText;	//Textコンポーネント取得用
+	private int isNewRecord;	//NewRecord flag用
+
+	void Start () {
+		//NewRecord flag用 0 = false
+		isNewRecord = PlayerPrefs.GetInt("NewRecord");
+		if(isNewRecord == 0){
+			newRecordText.enabled = false;		//UI非表示
+		}
+	}
+}
